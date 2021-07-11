2392e766bf75c1488eb3a9fd79d5acdc0894b497
brudolce <brudolce@gmail.com>
2021-07-11 00:47:56 -0300

    base64 encoding

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 
-//got the header from my browser's request to the endpoint, was having some CORs issues, kein zeit bruder, just copied n pasted here
 export const get = {
     'authorization': `Basic ${Buffer.from('bruno:E9uhGKyifa9hYzInZuVVFBO').toString('base64')}`,
     'Content-Type': 'application/json',
